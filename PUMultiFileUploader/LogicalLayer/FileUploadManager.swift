5c310fd899fdcc98da3e837c76ddec19b173cc4d
Payal Umraliya <PayalUmraliya@users.noreply.github.com>
2024-03-16 14:45:29 +0530

    Update FileUploadManager.swift

--- PUMultiFileUploader/LogicalLayer/FileUploadManager.swift
+++ PUMultiFileUploader/LogicalLayer/FileUploadManager.swift
@@ -56,7 +56,7 @@ class FileUploadManager : NSObject {
         self.uploadTask?.cancel()
     }
     
-    func uploadFile(mimeType:String = "image/png",fileName:String = "\(UUID().uuidString).png",completionHandler: @escaping (Data , [String:Any]?, Error?, Bool) -> Void)
+    func uploadFile(mimeType:String = "image/png",fileName:String = "\(ProcessInfo().globallyUniqueString).png",completionHandler: @escaping (Data , [String:Any]?, Error?, Bool) -> Void)
     {
         if let galleyImgData = imgData {
             self.uploadStatus = .uploading
